fac6c5c76277d232575f560879ed11cb87514946
Henry <hudson.hm10282627@gmail.com>
2026-02-27 16:40:40 -0300

    livro_poo

---
+++ livro.py
@@ -0,0 +1,18 @@
+from item_biblioteca import ItemBiblioteca
+
+
+class Livro(ItemBiblioteca):
+    def __init__(self, codigo, titulo, ano, autor, paginas):
+        super().__init__(codigo, titulo, ano)
+        self.__autor = autor
+        self.__paginas = paginas
+
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+
+        print(f"LIVRO | Código: {self.get_codigo()} | "
+              f"Título: {self.get_titulo()} | "
+              f"Autor: {self.__autor} | "
+              f"Páginas: {self.__paginas} | "
+              f"Ano: {self.get_ano()} | "
+              f"Status: {status}")
